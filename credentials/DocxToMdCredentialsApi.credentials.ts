972af099ddaa84c364a574d89bedeff0d44dbd6f
Sergei Frangulov <sfrangulov@gmail.com>
2025-07-03 12:52:09 +0400

    Add DocxToMarkdown credentials API and update package.json

---
+++ credentials/DocxToMdCredentialsApi.credentials.ts
@@ -0,0 +1,8 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class DocxToMdCredentialsApi implements ICredentialType {
+	name = 'docxToMdCredentialsApi';
+	displayName = 'DocxToMarkdown Credentials API';
+	documentationUrl = 'https://github.com/sfrangulov/n8n-nodes-docx-to-md';
+	properties: INodeProperties[] = [];
+}
